77ceb25ed01ab2fab59c072ad68421cabb8f91bb
chekavar-inset <chekavar.inset@gmail.com>
2026-03-14 04:13:42 +0400

    project section to responsive in small devices

--- src/HOME/WORKS/Work.jsx
+++ src/HOME/WORKS/Work.jsx
@@ -11,28 +11,28 @@ function Work() {
   return (
     <div className={styles.Work}>
       <div className="container-fluid">
-        <div className="row mb-5 mx-5">
+        <div className="row mb-md-5 mx-md-5">
           {" "}
           <div className="col-md-6">
             <h1 className={styles.workText}>Projects</h1>
           </div>
         </div>
-        <div className="row  mb-5 h-30 d-flex justify-content-center">
-          <div className="col-md-4 text-center">
+        <div className="row  mb-md-5 h-30 d-flex justify-content-center">
+          <div className="col-md-4 text-md-center">
             <a href="https://www.behance.net/gallery/232225721/Bus-Hunt">
               <div className={styles.projectImages}>
                 <img src={bushunt} alt="bushunt" />
               </div>
             </a>
           </div>
-          <div className="col-md-4 text-center">
+          <div className="col-md-4 text-end text-md-center ">
             <a href="https://www.behance.net/gallery/234933749/EVOX">
               <div className={styles.projectImages}>
                 <img src={evox} alt="evox" />
               </div>
             </a>
           </div>
-          <div className="col-md-4 text-center">
+          <div className="col-md-4 text-md-center">
             {" "}
             <a href="https://www.behance.net/gallery/233199689/NIDRA">
               <div className={styles.projectImages}>
@@ -42,21 +42,21 @@ function Work() {
           </div>
         </div>
         <div className="row h-30 d-flex justify-content-center">
-          <div className="col-md-4 text-center">
+          <div className="col-md-4 text-end text-md-center">
             <a href="https://www.behance.net/gallery/230034595/Orvento-Event-Management-UI">
               <div className={styles.projectImages}>
                 <img src={orvento} alt="orvento" />
               </div>
             </a>
           </div>
-          <div className="col-md-4 text-center">
+          <div className="col-md-4 text-md-center">
             <a href="https://www.behance.net/gallery/228877529/paybee">
               <div className={styles.projectImages}>
                 <img src={paybee} alt="paybee" />
               </div>
             </a>
           </div>
-          <div className="col-md-4 text-center">
+          <div className="col-md-4 text-end text-md-center">
             <a href="https://www.behance.net/gallery/229832893/Terrapod-Indoor-Plant-Brand-Identity-UI-Design">
               <div className={styles.projectImages}>
                 <img src={terrapod} alt="terrapod" />
